cac533e3e447559da88ae80e35b19f962e44f2e6
Maxi Learner <sagmeister@gmail.com>
2019-03-14 23:56:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,15 @@ function nowServing(katzDeliLine) {
     return "There is nobody waiting to be served!"
   }
 }
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  }
+  else {
+    var currentLine = []
+    for (let i = 1; i++)
+    currentLine.push(`${i}. &{katzDeliLine[i]})
+  }
+  return `The line is currently: %{currentLine}`
+}
