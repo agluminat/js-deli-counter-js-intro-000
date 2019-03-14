e5883fd1db480c2642f4cdaa332044a33c3f3023
Maxi Learner <sagmeister@gmail.com>
2019-03-14 23:59:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,13 +15,14 @@ function nowServing(katzDeliLine) {
   }
 }
 
+
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
   }
   else {
     var currentLine = []
-    for (let i = 1; i++)
+    for (let i = 1; i <= katzDeliLine.length; i++) 
     currentLine.push(`${i}. ${katzDeliLine[i]}`)
   }
   return `The line is currently: ${currentLine}`
